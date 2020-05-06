85a52594ff63a0b3c62aa8698c0546e5de4f5fb5
bush261800 <Sha@12345678>
2020-05-06 12:12:45 +0530

    Added initial commit to youtube player

---
+++ app/src/main/java/com/bush/androidyoutubeplayer/MainActivity.java
@@ -0,0 +1,19 @@
+package com.bush.androidyoutubeplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        YouTubePlayerView youTubePlayerView = findViewById(R.id.youtube_player_view);
+        getLifecycle().addObserver(youTubePlayerView);
+    }
+}
